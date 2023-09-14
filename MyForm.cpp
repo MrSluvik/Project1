11f62cd98b73ca0368ca99913c0accce8fe499ea
onushukvyatcheslav <onushukvyatcheslav@gmail.com>
2023-09-14 22:20:48 +0300

    Add button_click and MyForm_Click

--- MyForm.cpp
+++ MyForm.cpp
@@ -1,13 +1,21 @@
 #include "MyForm.h"
+#include<Windows.h>
 
 using namespace System;
 using namespace System::Windows::Forms;
 [STAThreadAttribute]
 
-int main(array<String^>^ args)
+int main(cli::array<String^> ^ args)
 {
 	Application::SetCompatibleTextRenderingDefault(false);
 	Application::EnableVisualStyles();
 	Project1::MyForm form;
 	Application::Run(% form);
 }
+//int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR, int) {
+//	Application::EnableVisualStyles();
+//	Application::SetCompatibleTextRenderingDefault(false);
+//	Project1::MyForm form;
+//	Application::Run(% form);
+//	return 0;
+//}
--- MyForm.h
+++ MyForm.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <string>
+#include<stdlib.h>
+#include<time.h>
+using namespace std;
 
 namespace Project1 {
 
@@ -34,6 +38,10 @@ namespace Project1 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button1;
+	protected:
 
 	private:
 		/// <summary>
@@ -48,12 +56,77 @@ namespace Project1 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(86, 12);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(120, 20);
+			this->textBox1->TabIndex = 0;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(124, 53);
+			this->label1->MaximumSize = System::Drawing::Size(50, 30);
+			this->label1->MinimumSize = System::Drawing::Size(40, 20);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(40, 20);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"label1";
+			// 
+			// button1
+			// 
+			this->button1->AutoEllipsis = true;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Modern No. 20", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->ForeColor = System::Drawing::SystemColors::HotTrack;
+			this->button1->Location = System::Drawing::Point(86, 87);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(120, 23);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Копіювання";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->AutoSize = true;
+			this->BackColor = System::Drawing::Color::Teal;
+			this->ClientSize = System::Drawing::Size(284, 261);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Click += gcnew System::EventHandler(this, &MyForm::MyForm_Click);
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
-#pragma endregion
-	};
+#pragma endregion	
+	private: System::Void button_Click(System::Object^ sender, System::EventArgs^ e)
+	{
+		this->label1->Text = this->textBox1->Text;
+
+	}
+	public: System::Void MyForm_Click(System::Object^ sender, System::EventArgs^ e)
+	{
+		this->label1->Text = "Початок роботи";
+		this->textBox1->Text="";	
+		
+		this->BackColor = System::Drawing::Color::
+		
+	}	
+	
+
+
+};
 }
